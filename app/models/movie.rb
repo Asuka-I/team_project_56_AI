67ccb404520614c1e218b66fcbf1dd3db9ad96d5
kohei <shoot0321@gmail.com>
2021-07-15 11:18:56 +0900

    Text,Movieモデルのカラムを「列挙型」に設定

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,15 @@
 class Movie < ApplicationRecord
   with_options presence: true do
-    validates :genru
+    validates :genre
     validates :title
     validates :url
   end
+  enum genre: {
+    invisible: 0,
+    basic: 1,
+    git: 2,
+    ruby: 3,
+    rails: 4,
+    php: 5
+  }
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,7 +1,15 @@
 class Text < ApplicationRecord
   with_options presence: true do
-    validates :genru
+    validates :genre
     validates :title
     validates :content
   end
+  enum genre: {
+    invisible: 0,
+    basic: 1,
+    git: 2,
+    ruby: 3,
+    rails: 4,
+    php: 5
+  }
 end
