5caea1ab29309e8381dcb8d7ed49972b4f40d319
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-05 20:53:58 +0900

    不備修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    if params[:genre] == 5
-      @movies = Movie.where(gerne: Movie::PHP_GENRE_LIST)
+    if params[:genre] == "php"
+      @movies = Movie.where(genre: Movie::PHP_GENRE_LIST)
     else
       @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
     end
