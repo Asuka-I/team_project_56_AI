49883e4c58bcda3e27c001a730f6ae9609ca5219
kohei <shoot0321@gmail.com>
2021-08-06 11:18:49 +0900

    watch_progressesコントローラにアクションを定義

--- app/controllers/watch_progresses_controller.rb
+++ app/controllers/watch_progresses_controller.rb
@@ -1,2 +1,11 @@
 class WatchProgressesController < ApplicationController
+  def create
+    current_user.watch_progresses.create!(movie_id: params[:movie_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.watch_progresses.find_by(movie_id: params[:movie.id]).destroy!
+    redirect_back(fallback_location: root_path)
+  end
 end
