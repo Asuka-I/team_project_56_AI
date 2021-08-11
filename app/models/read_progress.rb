f63ae4aece870f37aca29ef0aa452e5c8c9a2625
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-11 18:12:04 +0900

    rubocop -a を実行

--- app/models/read_progress.rb
+++ app/models/read_progress.rb
@@ -3,7 +3,7 @@ class ReadProgress < ApplicationRecord
   belongs_to :text
 
   validates :user_id, uniqueness: {
-                        scope: :text_id,
-                        message: "は同じテキスト教材を2回以上読破済みにはできません。",
-                      }
+    scope: :text_id,
+    message: "は同じテキスト教材を2回以上読破済みにはできません。"
+  }
 end
